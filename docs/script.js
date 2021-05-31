80249798399f051cdf936240f76442e7d7f3f53b
mtbishop <bishopmatthew76@gmail.com>
2021-05-31 12:42:36 -0500

    switched method

--- docs/script.js
+++ docs/script.js
@@ -1,72 +1,170 @@
 var generateBtn = document.querySelector('#generate');
-// String of characters that is randomized
-var allCharacters =
-  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+=[]{}|.`:;<'>?";
-var passwordLength = '';
 
-// for loop that randomizes the size of what the user chose
-function generatePassword(randomPassword) {
-  var results = '';
-  randomPassword, passwordLength;
-  for (var index = 0; index < randomPassword; index++) {
-    results += passwordLength.charAt(
-      Math.floor(Math.random() * passwordLength.length)
-    );
-  }
-  return results;
-}
-// main function that pushes the prompts to the user
+// Write password to the #password input
 function writePassword() {
-  passwordLength = '';
-  var userInput = parseInt(
-    prompt(
-      'How long would you like your password to be? (>8 or <128 characters)'
-    )
-  );
-  if (userInput >= 8 && userInput <= 128) {
-    var lowerCase = confirm(
-      'Do you want your password to contain lower case characters?'
+  //Prompts user with password criteria
+
+  //Do while loop is used to ensure that a password is inputted into the prompt text between the set boundaries.
+  do {
+    var passwordLength = parseInt(
+      prompt('Please pick a password length between 8 and 128 characters long.')
     );
-    var upperCase = confirm(
-      'Do you want your password to contain upper case characters?'
-    );
-    var numberCase = confirm('Do you want your password to contain numbers?');
-    var specialCase = confirm(
-      'Do you want your password to contain special characters?'
-    );
-    // if the user selects cancel/no to the first prompt that's asking to enter a password length
-  } else if (userInput === null) {
-    return;
-    // if the user chooses a character outside of the specified range
-  } else if (userInput < 8 || userInput > 128) {
-    alert('Please pick a number between 8 and 128');
-    return;
-  } 
-  //if they choose "Confirm" for lower case letters to be in their password
-  if (lowerCase) {
-    passwordLength += allCharacters.substring(0, 26);
-  }
-  //if they choose "Confirm" for upper case letter to be in their password
-  if (upperCase) {
-    passwordLength += allCharacters.substring(26, 52);
-  }
-  //if they choose "Confirm" for numbers to be in their password
-  if (numberCase) {
-    passwordLength += allCharacters.substring(52, 62);
-  }
-  //if they choose "Confirm" for special characters to be in their password
-  if (specialCase) {
-    passwordLength += allCharacters.substring(62);
-  }
-    // if the user says cancel/no to all prompts
-  else {
-    alert('Please select at least one option');
+    if (
+      passwordLength === null ||
+      Number.isInteger(passwordLength) === false ||
+      passwordLength < 8 ||
+      passwordLength > 128
+    ) {
+      alert('Please only input a whole number that is between 8 and 128.');
+      return;
+    }
+  } while (passwordLength < 8 || passwordLength > 128);
+  var lowercase = confirm('Would you like to include lowercase letters?');
+  var uppercase = confirm('Would you like to include uppercase letters?');
+  var numbers = confirm('Would you like to include numbers?');
+  var specialChars = confirm('Would you like to include special characters?');
+
+  //If cancel is selected for all , then the alert below is displayed and the password generation is ended.
+  if (
+    lowercase === false &&
+    uppercase === false &&
+    numbers === false &&
+    specialChars === false
+  ) {
+    alert('Must select at least one criteria option!');
     return;
   }
-  // this links the user input to the id that displays the password to the user
-  var password = generatePassword(userInput);
+
+  //Empty criteria array and all other criteria arrays are initialized.
+  var criteria = [];
+  var lowercaseArray = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ];
+  var uppercaseArray = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ];
+  var numbersArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var specialCharsArray = [
+    '!',
+    '#',
+    '$',
+    '%',
+    '&',
+    '(',
+    ')',
+    '*',
+    '+',
+    ',',
+    '-',
+    '.',
+    ':',
+    ';',
+    '<',
+    '=',
+    '>',
+    '?',
+    '@',
+    '^',
+    '_',
+    '|',
+    '~',
+  ];
+  var passwordString = '';
+  var count = 0;
+
+  //Pushes our various criteria specific arrays to our blank criteria array if user confirms they want them included.
+  if (lowercase) {
+    criteria.push(...lowercaseArray);
+    passwordString +=
+      criteria[Math.floor(Math.random() * lowercaseArray.length)]; //Pushes random character from this criteria to the overall criteria array to ensure a character makes it into the password in the end regardless of random chance.
+    count++;
+  }
+  if (uppercase) {
+    criteria.push(...uppercaseArray);
+    passwordString +=
+      criteria[Math.floor(Math.random() * uppercaseArray.length)];
+    count++;
+  }
+  if (numbers) {
+    criteria.push(...numbersArray);
+    passwordString += criteria[Math.floor(Math.random() * numbersArray.length)];
+    count++;
+  }
+  if (specialChars) {
+    criteria.push(...specialCharsArray);
+    passwordString +=
+      criteria[Math.floor(Math.random() * specialCharsArray.length)];
+    count++;
+  }
+
+  //Calls the generatePassword function and saves return value to variable called password.
+  var password = generatePassword();
+
+  //Possibly puts the generated password into the text area on the page???
   var passwordText = document.querySelector('#password');
   passwordText.value = password;
+
+  //Function that uses a for loop that iterates the length of the password desired by user.
+  //It generates a random character/letter/number each iteration based on criteria array built earlier and appends to new password array.
+  function generatePassword() {
+    for (i = 0; i < passwordLength - count; i++) {
+      var randomCharacter = Math.floor(Math.random() * criteria.length);
+      // Pushes randomCharacter to our password array each iteration.
+      passwordString += criteria[randomCharacter];
+    }
+    return passwordString;
+  }
 }
 
+// Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
